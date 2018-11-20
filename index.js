cd297265fae1aaab817d9b6bba94ea4a28c9e71f
Leandro Gonçalves <contato.leandrogoncalves@gmail.com>
2018-11-20 19:24:46 -0200

    add view

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const electron = require('require');
+const electron = require('electron');
 
 //import electron from 'electron' //ES6
 //Desestruturacao do obj electron
 
-const {app} = electron;
+const {app, BrowserWindow} = electron;
 
 
 //manipulando evento de inicio do app
 app.on('ready', (event) => {
-    console.log('app started succefuly');
-    console.log(event);
+    const mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
